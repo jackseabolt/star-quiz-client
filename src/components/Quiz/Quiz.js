7ef1d9c495fb1df500504b9025a281d2d10a2e48
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-15 12:13:36 -0800

    quiz component displaying data

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux'; 
 
 export class Quiz extends React.Component {
-    render() {   
+    render() {  
+        
+        let answers = this.props.answers.map((answer, idx) => (
+            <li key={idx}>{answer}</li>
+        )); 
+
         return ( 
             <div>
-                <p>Quiz</p>
+                <h2>{this.props.title}</h2>
+                <h3>{this.props.currentQuestion}</h3>
+                <ul>
+                {answers}
+                </ul>
             </div>
         )        
     }
 }
 
 const mapStateToProps = state => ({
-    quizActive: state.answers.length > 0
+    answers: state.answers, 
+    currentQuestion: state.currentQuestion, 
+    title: state.currentQuiz, 
+
 }); 
 
 export default connect(mapStateToProps)(Quiz); 
