fbaa15bed6993d2b0fcb17bdaaed462bd901f759
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-16 07:37:58 -0800

    score added to quiz

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -43,7 +43,7 @@ export class Quiz extends React.Component {
         )); 
 
         // Question or answer
-        let correctAnswer = this.props.response ? <h4 className={responseClasses.join(' ')}>{this.props.response}</h4> : <p><strong>QUESTION:</strong> {this.props.currentQuestion}</p>; 
+        let correctAnswer = this.props.response ? <div><h4 className={responseClasses.join(' ')}>{this.props.response}</h4><h3 className="quiz-score">{this.props.score}/{this.props.currentIndex}</h3></div> : <p><strong>QUESTION:</strong> {this.props.currentQuestion}</p>; 
 
         // Submit or next 
         let button = this.props.correctAnswer ? <button className="quiz-button-submit">Next</button> : <button className="quiz-button-submit">Submit</button>; 
@@ -86,7 +86,10 @@ const mapStateToProps = state => ({
     sessionId: state.sessionId,  
     correctAnswer: state.correctAnswer, 
     response: state.response,
-    continue: state.continue
+    continue: state.continue, 
+    quizLength: state.quizLength,
+    score: state.score, 
+    currentIndex: state.currentIndex
 }); 
 
 export default connect(mapStateToProps)(Quiz); 
