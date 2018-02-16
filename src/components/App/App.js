af129427016b8adf0a6504ed36851182b73125ed
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-16 14:00:46 -0800

    BoardHeader added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Footer from '../Footer/Footer';
 import Board from '../Board/Board'; 
 import Quiz from '../Quiz/Quiz'; 
 import Welcome from '../Welcome/Welcome';
+import BoardHeader from '../BoardHeader/BoardHeader';
 import {Route, withRouter} from 'react-router-dom';
 import './App.css';
 
@@ -22,6 +23,7 @@ export class App extends Component {
         { quiz }
         <Header />
         <Welcome />
+        <BoardHeader />
         <Route exact path="/" component={Board} />
         <Route exact path="/quiz" component={Quiz} />
         <Footer />
