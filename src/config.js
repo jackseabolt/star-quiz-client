e29889ecee7ff656106e38d08cc470dcee3c0e42
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-02-20 14:59:36 -0800

    test

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@
 console.log(process.env)
 
 
-if (process.env.NODE_ENV === 'development'){
-    module.exports = {
-        API_BASE_URL: 'http://localhost:8080'
-    }  
-} else {
+// if (process.env.NODE_ENV === 'development'){
+//     module.exports = {
+//         API_BASE_URL: 'http://localhost:8080'
+//     }  
+// } else {
     module.exports = {
         API_BASE_URL: 'https://star-quizes-api.herokuapp.com'
     }
-}
+// }
 
 
 
