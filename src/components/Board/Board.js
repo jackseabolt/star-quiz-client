4db2231e2096ffd0b3fd73e8c1f568c825ba7850
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-03-20 18:18:26 -0700

    pulse loader removed travis

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,27 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux'; 
 import './Board.css';  
 import { getAllQuizes, getNewQuiz } from '../../actions/quiz'; 
-import { PulseLoader} from 'react-spinners';
 
 export class Board extends React.Component {
     componentDidMount() {
         this.props.dispatch(getAllQuizes()); 
     }
     
-    render() {
-        // let boardPosts; 
-        // if(this.props.quizLoading){ 
-        //     boardPosts = <div className="board-loader">
-        //         <PulseLoader
-        //             color={'#fff'} 
-        //             loading={true} 
-        //         />
-        //     </div>
-        // } else {
-        //     boardPosts = this.props.quizes.map((quiz, index) => (
-        //         <BoardPost key={index} quiz={quiz} />
-        //     ))
-        // }   
+    render() { 
 
         return ( 
             <div className="board"> 
